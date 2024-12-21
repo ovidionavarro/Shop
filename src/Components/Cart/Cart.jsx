e747d2304b2d3191469b91c8956fc0971813e067
ovidionavarro <ovidionavarro90@gmail.com>
2024-12-21 09:43:28 -0500

    discount,remove and clear cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,11 +1,20 @@
 import { UseCart } from '../../Hooks/UseCart';
 
 export const Cart = () => {
-  const { cart, addToCart } = UseCart();
+  const { cart, addToCart,removeFromCart,
+    discountCart,clearCart } = UseCart();
 
   return (
     
-    <div className="p-4">
+    <>
+      <header>
+      <button 
+          className="px-2 py-1 bg-green-500 text-white rounded" 
+          onClick={() => clearCart()}>
+          Remove
+      </button>
+      </header>
+      <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Shopping Cart</h1>
       {cart.length === 0 ? (
         <p>Your cart is empty</p>
@@ -19,12 +28,12 @@ export const Cart = () => {
                 <p className="text-gray-600">${item.product.price}</p>
               </div>
               <div className="flex items-center space-x-2 mt-4">
-                {/* <button 
+                <button 
                   className="px-2 py-1 bg-gray-200 rounded" 
-                  onClick={() => decreaseQuantity(item.product.id)}
+                  onClick={() => discountCart(item.product)}
                 >
                   -
-                </button> */}
+                </button>
                 <span>{item.quantity}</span>
                 <button 
                   className="px-2 py-1 bg-gray-200 rounded" 
@@ -32,17 +41,18 @@ export const Cart = () => {
                 >
                   +
                 </button>
-                {/* <button 
+                <button 
                   className="px-2 py-1 bg-red-500 text-white rounded" 
-                  onClick={() => removeItem(item.product.id)}
+                  onClick={() => removeFromCart(item.product)}
                 >
                   Remove
-                </button> */}
+                </button>
               </div>
             </li>
           ))}
         </ul>
       )}
     </div>
+    </>
   );
 };
